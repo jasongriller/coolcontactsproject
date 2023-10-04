be9a57639685a1bfbed9a7940cba20d37847560b
Milo Gilad <milo@milogilad.com>
2023-10-04 18:45:13 -0400

    Removing e.preventDefault

--- app/login.tsx
+++ app/login.tsx
@@ -12,7 +12,6 @@ export const Login = (props) =>
 
     const handleSubmit = (e) =>
     {
-        e.preventDefault();
         console.log(email);
         fetch("/api/SignIn.php", {
             method: "POST",
